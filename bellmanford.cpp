1f77008cd57677b735522ddf39a51a716f457b33
MOHIMENUL ISLAM RAZIB <48291714+RAZIB71@users.noreply.github.com>
2019-04-27 10:29:18 +0600

    2.2 lab

---
+++ bellmanford.cpp
@@ -0,0 +1,112 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+typedef pair<int, int> pi;
+
+vector<pi> adj[100];
+vector<int> dist(100, INT_MAX), parent(100, -1), path[100];
+bool noPath;
+
+void relax(int v, int u, int w)
+{
+  dist[v] = dist[u] + w;
+}
+
+bool bellmanford(int s, int v)
+{
+  dist[s] = 0;
+
+  for(int i=0; i<v-1; i++){
+    for(int j=0; j<v; j++){
+        for(vector<pi>::iterator i=adj[j].begin(); i!=adj[j].end(); i++){
+            int u = (*i).first;
+            int w = (*i).second;
+
+            if(dist[u] > dist[j]+w){
+                relax(u, j, w);
+                parent[u] = j;
+            }
+        }
+    }
+  }
+
+  for(int i=0; i<v; i++){
+    for(vector<pi>::iterator j=adj[i].begin(); j!=adj[i].end(); j++){
+        int u = (*j).first;
+        int w = (*j).second;
+
+        if(dist[u] > dist[i]+w) return false;
+    }
+  }
+
+  return true;
+}
+
+
+void getShortestPath(int x, int i)
+{
+  if(x==0){
+    path[i].push_back(0);
+    return;
+  }
+
+  if(x==-1){
+    noPath = true;
+    path[i].clear();
+    return;
+  }
+
+  getShortestPath(parent[x], i);
+  path[i].push_back(x);
+}
+
+void printShortestPath(int d)
+{
+  noPath = false;
+  getShortestPath(d, d);
+
+  if(noPath){
+    cout << "No path." << endl;
+    return;
+  }
+
+  cout << "Shortest Path: ";
+  for(vector<int>::iterator i=path[d].begin(); i!=path[d].end(); i++)
+    cout << *i << (i== --path[d].end()? "" : "->");
+
+  cout << endl << "Cost: " << dist[d] << endl;
+}
+
+int main()
+{
+  int v, e;
+
+  cout << "Enter number of vertex: " << endl;
+  cin >> v;
+
+  cout << "Enter number of edges: " << endl;
+  cin >> e;
+
+  cout << "Enter edges: " << endl;
+  for(int i=1; i<=e; i++){
+    int a, b, w;
+    cin >> a >> b >> w;
+    adj[a].push_back(make_pair(b, w));
+  }
+
+  if(!bellmanford(0, v)){
+    cout << "Negative cycle." << endl;
+    return 0;
+  }
+
+  int d;
+  cout << "Enter destination: " << endl;
+  cin >> d;
+
+  printShortestPath(d);
+
+  return 0;
+}
+
---
+++ prims.cpp
@@ -0,0 +1,81 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+
+using namespace std;
+
+typedef pair<int, int> pi;
+
+vector<pi> adj[100];
+vector<int> key(100, INT_MAX), parent(100, -1);
+vector<bool> visited(100, false);
+int weightMST;
+
+void prims(int s)
+{
+  priority_queue<pi, vector<pi>, greater<pi> > pq;
+
+  key[s] = 0;
+  pq.push(make_pair(0, s));
+
+  while(!pq.empty()){
+     pi p = pq.top();
+     pq.pop();
+
+     int u = p.second;
+
+     if(visited[u]) continue;
+
+     weightMST+=p.first;
+
+     for(vector<pi>::iterator i=adj[u].begin(); i!=adj[u].end(); i++){
+        int v = (*i).first;
+        int w = (*i).second;
+
+        if(!visited[v] && w < key[v]){
+         key[v] = w;
+         parent[v] = u;
+         pq.push(make_pair(w, v));
+        }
+
+     }
+
+     visited[u] = true;
+  }
+
+}
+
+void printMST(int v)
+{
+  cout << endl << "Edges of MST: " << endl << endl;
+  for(int i=0; i<v; i++)
+    if(parent[i]!=-1) cout << parent[i] << " - " << i << " : " << key[i] << endl;
+
+  cout << endl << "Weight of MST: " << weightMST << endl;
+}
+
+int main()
+{
+    int v, e, s;
+
+    cout << "Enter number of vertex: " << endl;
+    cin >> v;
+    cout << "Enter number of edges: " << endl;
+    cin >> e;
+
+    cout << "Enter edges and their weights: " << endl;
+
+    for(int i=1; i<=e; i++){
+        int a, b, w;
+        cin >> a >> b >> w;
+        adj[a].push_back(make_pair(b, w));
+    }
+
+    cout << "Enter source: " << endl;
+    cin >> s;
+
+    prims(s);
+    printMST(v);
+
+  return 0;
+}
